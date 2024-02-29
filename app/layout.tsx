cbc57cad13d84354694f61ce6c89908cb3fbb341
achyutbenz19 <achyut.benz@gmail.com>
2024-02-28 20:48:26 -0500

    Revamp reop

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { cn } from "@/lib/utils";
+
+const font = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Wiscord",
+  description: "Communicate with a W",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          storageKey="wiscord-theme"
+        >
+          <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+            {children}
+          </body>
+        </ThemeProvider>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["uploadthing.com", "utfs.io"],
+  },
+};
+
+export default nextConfig;
