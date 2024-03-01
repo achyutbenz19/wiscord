9defafd7f90430ba28319de8c95257849b6e5294
achyutbenz19 <achyut.benz@gmail.com>
2024-03-01 14:13:37 -0500

    navigation actions + items

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const ServerIdPage = () => {
-  return (
-    <div>ServerIdPage</div>
-  )
-}
+  return <div>ServerIdPage</div>;
+};
 
-export default ServerIdPage
+export default ServerIdPage;
