3786256b217a1b03b924735a74b02df98a36c07d
achyutbenz19 <achyut.benz@gmail.com>
2024-03-01 19:43:13 -0500

    server sidebar beta

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,14 +1,26 @@
+import { currentProfile } from "@/lib/current-profile";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { redirect } from "next/navigation";
 import React from "react";
-import { string } from "zod";
+import { Separator } from "../ui/separator";
 
 interface ServerSidebarProps {
   serverId: string;
 }
 
-const ServerSidebar = ({ serverId }: ServerSidebarProps) => {
+const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    redirect("/");
+  }
+
   return (
-    <div>
-        {serverId}
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">{serverId}</div>
+        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+      </ScrollArea>
     </div>
   );
 };
