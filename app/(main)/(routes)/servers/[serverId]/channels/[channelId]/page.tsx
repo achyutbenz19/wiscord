546da9361e23b022f7856355ce02c2b0eb1a2e7c
achyutbenz19 <achyut.benz@gmail.com>
2024-03-17 14:08:47 -0400

    chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ChannelIdPage = () => {
-  return <div>ChannelIdPage</div>;
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
