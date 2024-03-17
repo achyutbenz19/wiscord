125e064b4da9533be574cda693a7def35d9306e7
achyutbenz19 <achyut.benz@gmail.com>
2024-03-17 13:21:25 -0400

    channel id page

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ServerIdPage = () => {
-  return <div>ServerIdPage</div>;
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") return null;
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel.id}`);
 };
 
 export default ServerIdPage;
